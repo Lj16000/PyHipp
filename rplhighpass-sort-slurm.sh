8230f67d41696e14724637240927ea6b26d9fa1f
AWS ParallelCluster user <ec2-user@ip-10-0-4-45.ap-southeast-1.compute.internal>
2025-09-07 17:47:56 +0000

    Use per-job conda env via envlist in rplhighpass-sort-slurm.sh

--- rplhighpass-sort-slurm.sh
+++ rplhighpass-sort-slurm.sh
@@ -7,6 +7,14 @@
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 
+/data/miniconda3/bin/conda init
+source ~/.bashrc
+envarg=$(/data/src/PyHipp/envlist.py)
+conda activate "$envarg"
+
 echo "Start:" $(date)
 python -u -c "import PyHipp as pyh; import time; pyh.RPLHighPass(saveLevel=1); from PyHipp import mountain_batch; mountain_batch.mountain_batch(); from PyHipp import export_mountain_cells; export_mountain_cells.export_mountain_cells(); print(time.localtime());"
 echo "End:" $(date)
+
+conda deactivate
+/data/src/PyHipp/envlist.py "$envarg"
